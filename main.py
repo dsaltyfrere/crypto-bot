5a9fb78cb1fadf89074f7f61338cd0a02d3e1b91
Nickolas D <nickolas.dury@protonmail.com>
2022-12-02 09:09:39 +0100

    Add filter.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 
 from telegram import __version__ as TG_VER
 from telegram.ext import Application, CommandHandler
+from telegram.ext.filters import Filters
 
 from commands.start import start as start
 from commands.fear import fear as fear
@@ -72,29 +73,31 @@ def main() -> None:
         raise RuntimeError(
             f"No TELEGRAM_BOT_TOKEN specified"
         )
+
     application = Application.builder().token(token).build()
+    filter = Filters.user(username=('@phyrxia'))
 
     #Add handlers
-    application.add_handler(CommandHandler(["start", "help"], start))
-    application.add_handler(CommandHandler(["fear", "f"], fear))
+    application.add_handler(CommandHandler(["start", "help"], start, filters=filter))
+    application.add_handler(CommandHandler(["fear", "f"], fear, filters=filter))
     #RSS
-    application.add_handler(CommandHandler(["list_feeds", "lf"], list))
-    application.add_handler(CommandHandler(["remove_feed", "rf"], remove))
-    application.add_handler(CommandHandler(["edit_feed", "ef"], edit))
-    application.add_handler(CommandHandler(["add_feed", "af"], add))
-    application.add_handler(CommandHandler(["set_feed_preview", "sfp"], preview))
+    application.add_handler(CommandHandler(["list_feeds", "lf"], list, filters=filter))
+    application.add_handler(CommandHandler(["remove_feed", "rf"], remove, filters=filter))
+    application.add_handler(CommandHandler(["edit_feed", "ef"], edit, filters=filter))
+    application.add_handler(CommandHandler(["add_feed", "af"], add, filters=filter))
+    application.add_handler(CommandHandler(["set_feed_preview", "sfp"], preview, filters=filter))
 
     # Whalepool
-    application.add_handler(CommandHandler(["add_whalepool_symbol", "aws"], add_whalepool_symbol))
-    application.add_handler(CommandHandler(["list_whalepool_symbol", "lws"], list_whalepool_symbol))
-    application.add_handler(CommandHandler(["remove_whalepool_symbol", "rws"], remove_whalepool_symbol))
-    application.add_handler(CommandHandler(["add_whalepool_transaction_type", "awtt"], add_whalepool_transaction_type))
-    application.add_handler(CommandHandler(["list_whalepool_transaction_type", "lwtt"], list_whalepool_transaction_type))
-    application.add_handler(CommandHandler(["remove_whalepool_transaction_type", "rwtt"], remove_whalepool_transaction_type))
+    application.add_handler(CommandHandler(["add_whalepool_symbol", "aws"], add_whalepool_symbol, filters=filter))
+    application.add_handler(CommandHandler(["list_whalepool_symbol", "lws"], list_whalepool_symbol, filters=filter))
+    application.add_handler(CommandHandler(["remove_whalepool_symbol", "rws"], remove_whalepool_symbol, filters=filter))
+    application.add_handler(CommandHandler(["add_whalepool_transaction_type", "awtt"], add_whalepool_transaction_type, filters=filter))
+    application.add_handler(CommandHandler(["list_whalepool_transaction_type", "lwtt"], list_whalepool_transaction_type, filters=filter))
+    application.add_handler(CommandHandler(["remove_whalepool_transaction_type", "rwtt"], remove_whalepool_transaction_type, filters=filter))
 
     # Jobs
-    application.add_handler(CommandHandler(["list_jobs", "lj"], list_jobs))
-    application.add_handler(CommandHandler(["update_job", "uj"], update_job))
+    application.add_handler(CommandHandler(["list_jobs", "lj"], list_jobs, filters=filter))
+    application.add_handler(CommandHandler(["update_job", "uj"], update_job, filters=filter))
 
     # Error handler
     application.add_error_handler(error_handler)
